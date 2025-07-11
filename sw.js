4fada6deb74480b619369070b27551dc5711a5dd
ShuyanMC <1149461164@qq.com>
2025-07-11 14:22:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,25 @@
-const CACHE_NAME = 'hidden-player-v2';
+const CACHE_NAME = 'hidden-player-v1';
 const CACHE_URLS = [
     '/',
-    '/oiiaio.html',
     'https://www.bilibili.com/video/BV1btJHzGEUE'
 ];
 
-// 安装时缓存资源
-self.addEventListener('install', event => {
+// 安装Service Worker
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(CACHE_URLS))
-});
-
-// 拦截请求
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(function(cache) {
+                return cache.addAll(CACHE_URLS);
+            })
     );
 });
 
-// 接收消息
-self.addEventListener('message', event => {
-    if (event.data.type === 'heartbeat') {
-        // 处理心跳消息
-        console.log('收到心跳:', event.data.time);
-    }
+// 拦截请求
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
 });
